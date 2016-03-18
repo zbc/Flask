490c116202a633c04847b01d9a7aa7493900a1a5
zbc <zhaobocong@hotmail.com>
2016-03-18 05:58:14 -0500

    03172016

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app, lm, oid, db
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
-from forms import LoginForm
+from forms import LoginForm, EditForm
 from models import User
 from datetime import datetime
 
@@ -35,6 +35,40 @@ def login():
         return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html', title = 'Sign In', form = form, providers = app.config['OPENID_PROVIDERS'])
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/user/<nickname>')
+def user(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user is None:
+        flash('User %s is not found' % user)
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body':'Test post #1'},
+        {'author': user, 'body':'Test post #2'},
+        {'author': user, 'body':'Test post #3'}
+    ]
+    return render_template('user.html', user = user, posts = posts)
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form = form)
+
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -66,21 +100,3 @@ def before_request():
         g.user.last_seen = datetime.utcnow()
         db.session.add(g.user)
         db.session.commit()
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-@app.route('/user/<nickname>')
-def user(nickname):
-    user = User.query.filter_by(nickname = nickname).first()
-    if user is None:
-        flash('User %s is not found' % user)
-        return redirect(url_for('index'))
-    posts = [
-        {'author': user, 'body':'Test post #1'},
-        {'author': user, 'body':'Test post #2'},
-        {'author': user, 'body':'Test post #3'}
-    ]
-    return render_template('user.html', user = user, posts = posts)
