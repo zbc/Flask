2979cacd2056b7caa90c139cd51d00ac8fc0a9cc
zbc <GameStopRD@gamestoprd.local>
2016-03-18 14:25:59 -0500

    03182015

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 from app import db
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(64), index = True, unique = True)
@@ -8,7 +13,12 @@ class User(db.Model):
     posts = db.relationship('Post', backref ='author', lazy = 'dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
-
+    followed = db.relationship('User',
+                               secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
     @staticmethod
     def make_unique_nickname(nickname):
         if User.query.filter_by(nickname = nickname).first() is None:
@@ -20,7 +30,7 @@ class User(db.Model):
                 break
             version += 1
         return newnickname
-        
+
     @property
     def is_authenticated(self):
         return True
